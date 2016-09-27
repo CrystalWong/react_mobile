8611bb6112672c771a4813dc0a32fd5334c163c6
unknown <wang.yong@wangyong-pc.jyall.com>
2016-09-27 13:45:32 +0800

    代码简化

--- src/Config/Route.jsx
+++ src/Config/Route.jsx
@@ -1,16 +1,13 @@
 import React, {Component, PropTypes} from 'react';
 import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router';
 
-import IndexList from '../Component/IndexList'; //首页组件
-import Topic from '../Component/Topic'; //主题详情
-import MyMessages from '../Component/MyMessages'; //我的消息
-import UserView from '../Component/UserView'; //我的个人中心
-import Login from '../Component/Login'; //登录
+import Login from '../views/Login'; //登录
 import Register from '../views/Register'; //注册
 import RegisterPro from '../views/RegisterPro'; //注册协议
-import Signout from '../Component/Signout'; //退出
 import Yue from '../views/yue'; //预约
 import Download from '../views/Download'; //预约
+import RegisterPassword from '../views/RegisterPassword'; //设置密码
+import {Tool, merged} from '../Tool';
 
 /**
  * (路由根目录组件，显示当前符合条件的组件)
@@ -31,16 +28,13 @@ const RouteConfig = (
         <Route path="/" component={Roots}>
             <IndexRoute component={Login} />
             <Route path="register" component={Register} />
+            <Route path="registerpd" component={RegisterPassword} />
             <Route path="registerpro" component={RegisterPro} />
-            <Route path="topic/:id" component={Topic} />
-            <Route path="my/messages" component={MyMessages} />
-            <Route path="user/:loginname" component={UserView} />
-            <Route path="signin" component={IndexList} />
-            <Route path="signout" component={Signout} />
             <Route path="yue" component={Yue} />
             <Route path="download" component={Download} />
         </Route>
     </Router>
 );
+Tool.rem();
 
 export default RouteConfig;
