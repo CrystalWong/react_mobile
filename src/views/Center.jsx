4c6e5fd6d18303f3eab81e88b8f30006b6db64a7
vega <li.jianyong@jyall.com>
2016-10-08 17:36:01 +0800

    1、个人中心 展示用户信息 2、地址管理 功能完善

--- src/views/Center.jsx
+++ src/views/Center.jsx
@@ -1,19 +1,18 @@
 import React,{Component,PropTypes} from 'react';
 import Cookie from 'react-cookie';
+import URLS from '../constants/urls.js';
 import {Tool, merged} from '../Tool';
 import '../Style/center';
 
 class Center extends Component {
 	constructor(props){
 		super(props);
-
 		this.state = {
 			userId : Cookie.load('userId'),
-			avatarURL : Cookie.load('photo'),
+			photo : Cookie.load('photo'),
 			nickname : Cookie.load('name'),
 			bean : 0
 		};
-		console.log('用户ID：' + this.state.userId);
 		//获取家园豆
 		this.getBean = () => {
 			Tool.fetch(this,{
@@ -29,18 +28,17 @@ class Center extends Component {
 	//是否登录
 	noLogin(){
 		//检测是否登录，否跳转至登录页
-		if(true){
+		if(!this.state.userId){
 			window.location.href = '/'
 		}
 	}
 
 	componentWillMount(){
-		// this.noLogin();
+		this.noLogin();
 	}
 
 	componentDidMount() {
 	    this.getBean();
-	    // this.getBean();
 	}
 
 
@@ -48,14 +46,14 @@ class Center extends Component {
 		return(
 			<div>
 				<header className="center-header">
-					<div className="ch-avatar"><img src="{this.state.avatarURL}" /></div>
+					<div className="ch-avatar"><img src={this.state.photo} /></div>
 					<p className="ch-nickname">{this.state.nickname}</p>
 					<p className="ch-bean">家园豆：{this.state.bean}</p>
 				</header>
 				<ul className="center-menu">
 					<li><a href="/yue" className="cm-reservation">预约单</a></li>
-					<li><a href="#" className="cm-order">订单</a></li>
-					<li><a href="#" className="cm-address">管理收货地址</a></li>
+					<li><a href="/orderclosed" className="cm-order">订单</a></li>
+					<li><a href="/address" className="cm-address">管理收货地址</a></li>
 				</ul>
 			</div>
 		)
