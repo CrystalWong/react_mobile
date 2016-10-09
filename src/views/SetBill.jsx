a74ab96d8337184b519ec32af227764d94436bbd
xsd <xue.songde@jyall.com>
2016-10-09 17:56:39 +0800

    发票结算页面

--- src/views/SetBill.jsx
+++ src/views/SetBill.jsx
@@ -1,18 +1,45 @@
 import React, {Component, PropTypes} from 'react';
 import {Header} from '../Component/common/index';
-
+import '../Style/setbill';
 /**
  * 模块入口
  * 
- * @class SetBill
+ * @className SetBill
  * @extends {Component}
  */
 class SetBill extends Component {
     render() {
         return (
             <div>
-                <Header title="设置发票信息" leftIcon="fanhui" />
-                <div>内容</div>
+                <header className="common-header">
+                    <div className="left-arrow"><a><i></i></a></div>
+                    <h2 className="title">订单结算</h2></header>
+                    <div className="setBill">
+                    	<div className="fptype">
+			              	<h3>
+			              		发票类型
+			              	</h3>
+			              	<div className="typediv">
+			              		<a data-info="">不需要</a>
+			               		<a data-info="" className="active">电子发票</a>
+			               		<a data-info="">纸质发票</a>
+			              	</div>
+		               </div>
+		               <div className="fptype1">
+			              	<h3>
+			              		发票抬头
+			              	</h3>
+			              	<div className="typediv1">
+			              		<a data-info="per"><span className="checked"></span><span>个人</span></a>
+			               		<a data-info="comp"><span className="unchecked"></span><span>单位</span></a>
+			               		<p><input type="text" id="fptt"/></p>
+			              	</div>
+		               </div>
+		               <div className="bootm">
+							<a className="confpinfo">确定</a>
+						</div>
+                    </div>
+               
             </div>
         );
     }
