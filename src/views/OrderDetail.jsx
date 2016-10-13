d8d7d25267c9f5e4afbfc969544b1e64607519ea
fu.ruigang <fu.ruigang@jyall.com>
2016-10-13 18:36:41 +0800

    订单详情

--- src/views/OrderDetail.jsx
+++ src/views/OrderDetail.jsx
@@ -13,8 +13,46 @@ class OrderDetail extends Component {
         return (
             <div className="order-detail">
                 <Header title="全部订单" leftIcon="fanhui" />
-                <div>
-                    内容
+                <div className="num">
+                    <p>订单号:39483903948483030</p>
+                </div>
+                <div className="address1">
+                    <div className="adinfo">
+                    <p>张小明&nbsp;15121345566<span>默认</span></p>
+                        <span>地址：</span><span>北京市北京市东城区广渠门外南街8号楼金色家园大厦金色家园大厦</span>
+                    </div>
+                </div>
+                <div className="product">
+                    <ul>
+                        <li>
+                            <div>
+                                <span></span>
+                                <img src="src/images/orderdetail/chouyanji.png" />
+                            </div>
+                            <div>
+                                <h3>德意(DE&E)269E+170G抽烟机环保包邮</h3>
+                                <p>颜色分类：<span>银白色</span></p>
+                            </div>
+                            <div>
+                                <span>￥2180.00</span>
+                                <span>X1</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <span></span>
+                                <img src="src/images/orderdetail/chouyanji.png" />
+                            </div>
+                            <div>
+                                <h3>德意(DE&E)269E+170G抽烟机环保包邮</h3>
+                                <p>颜色分类：<span>银白色</span></p>
+                            </div>
+                            <div>
+                                <span>￥2180.00</span>
+                                <span>X1</span>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
