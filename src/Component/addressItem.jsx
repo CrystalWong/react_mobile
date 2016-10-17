1c3e0ee581b983c1f9ab5e2e4a1b3f4126f36b3d
xsd <xue.songde@jyall.com>
2016-10-17 10:11:36 +0800

    合并dev

--- src/Component/addressItem.jsx
+++ src/Component/addressItem.jsx
@@ -6,14 +6,11 @@ import {Tool, merged} from '../Tool';
 export class AddressItem extends Component{
 	constructor(props){
 		super(props);
-		this.state = {
-			addressType : this.props.type == 1 ? true : false
-		}
 	}
 
 	//设为默认地址
 	addressDefault(){
-		console.log(this)
+		this.props.callbackDefault(this);
 	}
 
 	//编辑地址
@@ -23,20 +20,19 @@ export class AddressItem extends Component{
 
 	//删除地址
 	addressDel(){
-		console.log('del')
+		this.props.callbackDel(this);
 	}
 
 	render(){
-		console.log(this.props)
 		let {consigneeName,consigneeMobile,detailInfo,type} = this.props;
 		return(
 			<li>
 				<div className="address-msg">
-					<h6><span>{consigneeName}</span><span>{consigneeMobile}</span>{this.state.addressType ? <em>默认</em> : ''}</h6>
+					<h6><span>{consigneeName}</span><span>{consigneeMobile}</span>{this.props.type==1 ? <em>默认</em> : ''}</h6>
 					<p>{detailInfo}</p>
 				</div>
 				<div className="address-operation">
-					<span className={this.state.addressType ? 'current on' : 'current'} onClick={this.addressDefault.bind(this)}>设为默认</span>
+					<span className={this.props.type==1? 'current on' : 'current'} onClick={this.addressDefault.bind(this)}>设为默认</span>
 					<span className="ao-del" onClick={this.addressDel.bind(this)}>删除</span>
 					<span className="ao-edit" onClick={this.addressEdit.bind(this)}>编辑</span>
 				</div>
--- src/Component/orderClosedList.jsx
+++ src/Component/orderClosedList.jsx
@@ -1,30 +1,22 @@
 import React, {Component, PropTypes} from 'react';
+import {OrderClosedListItem} from '../Component/orderClosedItem';
 /**
  * @export
  * @class AddReduce
  * @extends {Component}
  */
-export class orderClosedList extends Component {
-    
+export class OrderClosedList extends Component {
     render() {
-        console.log('订单子组件..');
-        console.log(this.props);
+        let {address,couponUserList,goodsTotalFee,orderTotalFee,storeVOList,totalShipFee} = this.props;
         return (
-                        <a className="tanm">
+            <div>
                             {
-
+                                storeVOList.map((item,index)=>
+                                    <OrderClosedListItem key={index} {...item}/>
+                                )
                             }
-                            <dl className="clearfix">
-                                <dt>
-                                    <img src="http://image1.jyall.com/v1/tfs/T1TqJ_BTWv1RXrhCrK.jpg"/>
-                                </dt>
-                                <dd>
-                                    <p>艾理思 美式实木真皮配布沙发 Y630-19<br/><span>颜色分类:<span>红色</span></span></p>
-                                <p className="price">¥ 9250.00<br/><span>x1</span></p>
-                                </dd>
-                            </dl>
-                         <div className="liu"><label>留言:</label><input type="text"/></div>
-                        </a>
+                            
+            </div>
         );
     }
 }
