c70c11310a7abb4e6193c8fcc9b9682c04da8fcb
lu <lu.hongtao@jyall.com>
2016-12-13 20:20:15 +0800

    预约接口字段调整

--- src/views/Appointment.jsx
+++ src/views/Appointment.jsx
@@ -197,8 +197,9 @@ var OrderList = React.createClass({
                infoAddress= '';
           tellNum = tellNum.replace(/(\d+)-(\d+)-(\d+)-(\d+)/, "$1$2$3,$4");
           if(variableMap.addressInfo){
-            let addressTwo = variableMap.addressInfo.doorNum?variableMap.addressInfo.doorNum:"";
-            infoAddress = addressTwo;
+            let addressOne = variableMap.addressInfo.community?variableMap.addressInfo.community:"",
+                addressTwo = variableMap.addressInfo.doorNum?variableMap.addressInfo.doorNum:"";
+            infoAddress = addressOne + addressTwo;
           }
           return (
                <div className="appointmentlist">
