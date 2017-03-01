6f1b07390f4a6af2ef55e00c0fdaf9c6b9b3bbaa
xsd <xue.songde@jyall.com>
2017-03-01 17:11:04 +0800

    优惠券

--- src/views/My-coupon.jsx
+++ src/views/My-coupon.jsx
@@ -28,7 +28,25 @@ class Mycoupon extends Component {
                scrollNoData: false, //分页没有数据
                display: '',
                nolist : 'block',
-               dataList : [],
+               unUsedCount :'',
+               usedCount :'',
+               expiredCount :'',
+               couponState:'active',
+               dataList:[]
+          }
+          this.getCouponBy=(e)=>{
+            this.refs.couponState.childNodes.forEach(function(item,index){
+              item.className='';
+            });
+            e.target.className='active';
+          }
+          this.couponScroll=(e)=>{
+            console.log(e.target.scrollHeight);//元素的高度
+            console.log(e.target.clientHeight);//窗口高度
+            console.log(e.target.scrollTop);//收进去的高度
+            if(e.target.clientHeight+e.target.scrollTop+54>=e.target.scrollHeight){
+              console.log('滚动..');
+            }
           }
           this.getAppointmentList = () => {
                let _this = this,
@@ -38,7 +56,17 @@ class Mycoupon extends Component {
                     type: "get",
                     headers:headers,
                     successMethod: function(json){
-
+                      _this.setState({
+                          dataList: json.data,
+                          unUsedCount: json.unUsedCount,
+                          usedCount:json.usedCount,
+                          expiredCount:json.expiredCount
+                      });
+                      if(json.data.length=10){
+                        _this.setState({
+                            pageNo: _this.state.pageNo++
+                        });
+                      }
                     },
 
                });
@@ -58,89 +86,83 @@ class Mycoupon extends Component {
 
      render() {
         return (
-                <div className="my-coupon">
+                <div className="my-coupon" onScroll={this.couponScroll.bind(this)}>
                 <Header title="我的优惠券" leftIcon="fanhui" hasRight="true"/>
-                  <div className="mo-nav">
-                      <ul className="clearfix">
-                          <li id="0" className="active">未使用(1)</li>
-                          <li id="10">已使用(2)</li>
-                          <li id="20">已过期(333)</li>
+                <div className="wrapdiv">
+                    <div className="mo-nav">
+                        <ul className="clearfix" onClick={this.getCouponBy.bind(this)} ref="couponState">
+                            <li id="0" className="active">未使用({this.state.unUsedCount})</li>
+                            <li id="10">已使用({this.state.usedCount})</li>
+                            <li id="20">已过期({this.state.expiredCount})</li>
+                        </ul>
+                    </div>
+                    <div className="activation">
+                        <input type="num" placeholder="请输入优惠券编码"/>
+                        <span>激活</span>
+                    </div>
+                    <div className="no-coupon" style={{display: this.state.dataList.length==0?'block':'none'}}>没有对应优惠券</div>
+                    <div className="coupon-list" style={{display: this.state.dataList.length==0?'none':'block'}}>
+                      <ul>
+                          {
+                                this.state.dataList.map((item,index) => 
+                                     <CouponList key={index}{...item} />
+                                )
+                           }
                       </ul>
-                  </div>
-                  <div className="activation">
-                      <input type="num" placeholder="请输入优惠券编码"/>
-                      <span>激活</span>
-                  </div>
-                  <div className="no-coupon">没有对应优惠券</div>
-                  <div className="coupon-list">
-                    <ul>
-                      <li>
-                        <div className="div-inline name-info">
-                          <div className="left-div"><h1>¥100</h1><span>(满减券)</span></div>
-                          <a href="">全品类<i></i></a>
-                        </div>
-                        <div className="div-inline date-info">
-                          <p>有效期:2017-2-27 14:33:44</p>
-                          <div className="right-div">满300使用</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="div-inline name-info">
-                          <div className="left-div"><h1>¥100</h1><span>(满减券)</span></div>
-                          <a href="">全品类<i></i></a>
-                        </div>
-                        <div className="div-inline date-info">
-                          <p>有效期:2017-2-27 14:33:44</p>
-                          <div className="right-div">满300使用</div>
-                        </div>
-                      </li>
-                    </ul>
+                    </div>
                   </div>
                   <div className="get-coupon-button">
                     领取更多好券<i></i>
                   </div>
+
                 </div>
         );
     }
 }
-var OrderList = React.createClass({
-     render: function() {
-          let {lastUpdateTime,variableMap} = this.props,
-               tellNum = variableMap.businessPeople.butler.mobile,
-               infoAddress= '';
-          tellNum = tellNum.replace(/(\d+)-(\d+)-(\d+)-(\d+)/, "$1$2$3,$4");
-          if(variableMap.addressInfo){
-            let addressOne = variableMap.addressInfo.community?variableMap.addressInfo.community:"",
-                addressTwo = variableMap.addressInfo.doorNum?variableMap.addressInfo.doorNum:"";
-            infoAddress = addressOne + addressTwo;
-          }
-          return (
-               <div className="appointmentlist">
-                    <h3>{variableMap.goodsInfo.goodsGroup.goodsGroupName}</h3>
-                    <p>
-                         预约时间：{lastUpdateTime} <br/>
-                         地址：{infoAddress} <br/>
-                         备注：{variableMap.appointmentParam.remark?variableMap.appointmentParam.remark:""}
-                    </p>
-                    <h3 className="generoname">您的专属管家：{variableMap.businessPeople.butler.empName}<a href={`tel:`+tellNum}><button>联系他</button></a></h3>
-                </div>
-          );
-     }
-});
-
-
-
-var NoList = React.createClass({
-  render: function() {
-    return (
-        <div style={{ display: this.props.display }} className="no-list">
-            <img src={require("../images/appointment/icon-appoint.png")} style={{width: '1.63rem'}} />
-            <p>预约单还是空的，去逛逛吧~ <br/></p>
-            <a href="http://m.jyall.com"><button>继续逛逛</button></a>
-        </div>
-    );
-  }
-});
-
 export default Mycoupon;
 process.env.NODE_ENV !== 'production' && module.hot.accept();
+//循环优惠券列表
+class CouponList extends Component {
+     constructor(props){
+      super(props);
+      this.subStrTime=(str)=>{
+            return str.split(' ')[0];
+      }
+     }
+     render(){
+          let {groupValue,groupType,couponName,goodsRange,startUseTime,endUseTime} = this.props;
+          return (
+                      <li>
+                        <div className="div-inline name-info">
+                          <div className="left-div"><h1>¥{groupValue}</h1><span>({groupType})</span></div>
+                          <a href="">{couponName}<i></i></a>
+                        </div>
+                        <div className="div-inline date-info">
+                          <p>有效期:{this.subStrTime(startUseTime)}-{this.subStrTime(endUseTime)}</p>
+                          <div className="right-div">{goodsRange}</div>
+                        </div>
+                      </li>
+          );
+     }
+}
+// var CouponList = React.createClass({
+//       console.log(this);
+//       subStrTime=(str)=>{
+//             return str.split(' ')[0];
+//       }
+//      render: function() {
+//           let {groupValue,groupType,couponName,goodsRange,startUseTime,endUseTime} = this.props;
+//           return (
+//                       <li>
+//                         <div className="div-inline name-info">
+//                           <div className="left-div"><h1>¥{groupValue}</h1><span>({groupType})</span></div>
+//                           <a href="">{couponName}<i></i></a>
+//                         </div>
+//                         <div className="div-inline date-info">
+//                           <p>有效期:{this.subStrTime(startUseTime)}-{endUseTime}</p>
+//                           <div className="right-div">{goodsRange}</div>
+//                         </div>
+//                       </li>
+//           );
+//      }
+// });
